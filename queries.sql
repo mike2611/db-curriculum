c52cee1dd30c0a0b4cc01796ed28e6b82c2573a9
mike2611 <mpuentes2611@gmail.com>
2021-12-01 15:02:23 -0600

    Add SQL to queries.sql

--- queries.sql
+++ queries.sql
@@ -1,13 +1,21 @@
 /*Queries that provide answers to the questions from all projects.*/
 
 SELECT * FROM animals WHERE name LIKE '%mon';
+
 SELECT name FROM animals WHERE date_of_birth > '2016-12-12' AND date_of_birth < '2019-01-01';
+
 SELECT name FROM animals WHERE neutered AND escape_attempts < 3;
+
 SELECT date_of_birth FROM animals WHERE name = 'Pikachu' OR name = 'Agumon';
+
 SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
+
 SELECT * FROM animals WHERE weight_kg > 10.5;
+
 SELECT * FROM animals WHERE neutered;
+
 SELECT * FROM animals WHERE name != 'Gabumon';
+
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
 
 
@@ -62,18 +70,72 @@ WHERE weight_kg < 0;
 COMMIT;
 
 SELECT COUNT(*) FROM animals;
+
 SELECT COUNT(*) 
 FROM animals
 WHERE escape_attempts = 0;
+
 SELECT AVG(weight_kg) FROM animals;
 SELECT neutered, SUM(escape_attempts)
 FROM animals
 GROUP BY neutered;
+
 SELECT species, MAX(weight_kg), MIN(weight_kg)
 FROM animals
 GROUP BY species;
+
 SELECT species, AVG(escape_attempts)
 FROM animals
 WHERE date_of_birth > '1990-12-31' AND date_of_birth < '2000-01-01'
 GROUP BY species;
 
+SELECT full_name, name
+FROM animals
+INNER JOIN owners
+ON owner_id = owners.id
+WHERE full_name LIKE 'Melody Pond';
+
+SELECT animals.name, species.name
+FROM animals
+INNER JOIN species
+ON species_id = species.id
+WHERE species.name LIKE 'Pokemon';
+
+SELECT full_name, name
+FROM animals
+FULL JOIN owners
+ON owner_id = owners.id;
+
+SELECT COUNT(species_id)
+FROM animals
+INNER JOIN species
+ON species_id = species.id
+WHERE species.name LIKE 'Pokemon';
+
+SELECT COUNT(species_id)
+FROM animals
+INNER JOIN species
+ON species_id = species.id
+WHERE species.name LIKE 'Digimon';
+
+
+SELECT full_name, name
+FROM animals
+INNER JOIN owners
+ON owner_id = owners.id
+WHERE full_name LIKE 'Jennifer Orwell' AND species_id = 2;
+
+SELECT full_name, name
+FROM animals
+INNER JOIN owners
+ON owner_id = owners.id
+WHERE full_name LIKE 'Dean Winchester' AND escape_attempts = 0;
+
+SELECT full_name, COUNT(full_name) AS value_occurrence 
+FROM owners
+INNER JOIN animals
+ON owners.id = owner_id
+GROUP BY full_name
+ORDER BY 
+  value_occurrence DESC
+LIMIT 1;
