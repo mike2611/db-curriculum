674180854ffd2b81f8156b294cf1c5fc35bea361
mike2611 <mpuentes2611@gmail.com>
2021-12-02 11:46:41 -0600

    Add SQL to schema.sql

--- schema.sql
+++ schema.sql
@@ -37,3 +37,26 @@ ALTER TABLE animals
 ADD COLUMN owner_id INT,
 ADD FOREIGN KEY(owner_id) 
 REFERENCES owners(id);
+
+CREATE TABLE vets(
+	id  INT GENERATED ALWAYS AS IDENTITY,
+	name VARCHAR(250),
+	age INT,
+	date_of_graduation DATE,
+	PRIMARY KEY(id)
+);
+
+
+CREATE TABLE specializations(
+ species_id INT REFERENCES species(id) ON UPDATE CASCADE ON  DELETE CASCADE,
+ vet_id INT REFERENCES species(id) ON UPDATE CASCADE ON DELETE CASCADE,
+ PRIMARY KEY (species_id, vet_id)
+);
+
+CREATE TABLE visits(
+ species_id INT REFERENCES species(id) ON UPDATE CASCADE ON  DELETE CASCADE,
+ vet_id INT REFERENCES species(id) ON UPDATE CASCADE ON DELETE CASCADE,
+ date_of_visit DATE,
+ PRIMARY KEY (species_id, vet_id)
+);
+
