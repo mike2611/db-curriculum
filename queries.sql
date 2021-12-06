f7d1d18a63d0dfb58ca11d010599f30f673adb31
mike2611 <mpuentes2611@gmail.com>
2021-12-06 10:58:33 -0600

    Add changes

--- queries.sql
+++ queries.sql
@@ -139,3 +139,90 @@ GROUP BY full_name
 ORDER BY 
   value_occurrence DESC
 LIMIT 1;
+
+SELECT animals.name, MAX(date_of_visit) AS last_day 
+FROM animals
+INNER JOIN visits
+ON animals.id = animals_id
+WHERE vet_id = (SELECT id FROM vets WHERE name = 'William Tatcher')
+GROUP BY name
+ORDER BY 
+  last_day DESC
+LIMIT 1;
+
+
+SELECT COUNT(animals_id) 
+FROM visits
+WHERE vet_id = (SELECT id FROM vets WHERE name = 'Stephanie Mendez');
+
+SELECT vets.name, (SELECT name FROM species WHERE id = specializations.species_id)
+FROM vets
+FULL JOIN specializations
+ON specializations.vet_id = vets.id;
+
+SELECT (SELECT name FROM animals WHERE id = visits.animals_id)
+FROM animals
+INNER JOIN visits
+ON animals.id = visits.animals_id
+WHERE vet_id = (SELECT id FROM vets WHERE name = 'Stephanie Mendez')
+AND date_of_visit > '2020-04-01' AND date_of_visit < '2020-08-30';
+
+SELECT (SELECT name FROM animals WHERE id = animals_id), 
+COUNT(animals_id) AS value_occurrence
+FROM visits
+GROUP BY animals_id
+ORDER BY 
+  value_occurrence DESC
+LIMIT 1;
+
+SELECT (SELECT name FROM animals WHERE id = visits.animals_id), date_of_visit
+FROM animals
+INNER JOIN visits
+ON animals.id = visits.animals_id
+WHERE vet_id = (SELECT id FROM vets WHERE name = 'Maisy Smith')
+ORDER BY date_of_visit
+LIMIT 1;
+
+SELECT (SELECT name FROM animals WHERE id = animals_id), (SELECT name FROM vets WHERE id = vet_id), date_of_visit
+FROM visits
+WHERE date_of_visit IN (SELECT max(date_of_visit) FROM visits);
+
+SELECT animals.species_id,  specializations.species_id
+FROM visits
+FULL JOIN specializations
+ON visits.vet_id = specializations.vet_id
+FULL JOIN animals
+ON visits.animals_id = animals.id
+WHERE visits.vet_id != (SELECT id FROM vets WHERE name = 'Stephanie Mendez') AND 
+(specializations.species_id != animals.species_id OR  specializations.species_id  IS NULL);
+
+SELECT (SELECT name FROM species WHERE id = animals.species_id), COUNT(animals.species_id) AS value_occurrence 
+FROM visits
+FULL JOIN animals
+ON visits.animals_id = animals.id
+WHERE visits.vet_id = (SELECT id FROM vets WHERE name = 'Maisy Smith')
+GROUP BY animals.species_id
+ORDER BY 
+  value_occurrence DESC
+LIMIT 1;
+
+SELECT
+  COUNT(visits.date_of_visit),
+  vets.name
+FROM
+  visits
+  JOIN vets ON visits.vet_id = vets.id
+  JOIN animals ON visits.animal_id = animals.id
+WHERE
+  NOT animals.species_id = ANY (
+    ARRAY(
+      SELECT
+        specializations.species_id
+      FROM
+        specializations
+      WHERE
+        specializations.vet_id = vets.id
+    )
+  )
+GROUP BY
+  vets.name;
